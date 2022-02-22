67b65518576a37cfaf6d90cf09a8c1ee07e8588b
Satya Karki <spacegrlsatya@gmail.com>
2022-02-22 20:42:53 +0600

    initial commit

---
+++ WelcomeNoteContract/WelcomeNote.cs
@@ -0,0 +1,27 @@
+﻿using Stratis.SmartContracts;
+using System;
+
+public class WelcomeNote : SmartContract
+{
+    public WelcomeNote(ISmartContractState smartContractState)
+    : base(smartContractState)
+    {
+        this.WelcomeMessage = "Welcome to Blockchain in C# Tutorials";
+    }
+
+    private string WelcomeMessage
+    {
+        get
+        {
+            return this.PersistentState.GetString("WelcomeMessage");
+        }
+        set
+        {
+            this.PersistentState.SetString("WelcomeMessage", value);
+        }
+    }
+    public string DisplayWelcomeNote()
+    {
+        return this.WelcomeMessage;
+    }
+}
